24f8eaf8a1813d8cf7484ff42a92613ac30a8015
Zouzouuu69 <lettezouzou3@gmail.com>
2025-04-09 16:31:07 +0200

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,34 +1,12 @@
-import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Vercel x Groq Chatbot</h1>
-      <p className="text-center">
-        This starter project uses <Link href="https://groq.com/">Groq</Link>{" "}
-        with the <Link href="https://sdk.vercel.ai/docs">AI SDK</Link> via the{" "}
-        <Link href="https://vercel.com/marketplace/groq">
-          Vercel Marketplace
-        </Link>
-        .
+      <h1 className="text-3xl font-semibold mb-4">⚡ L’IA du Seigneur Suprême ⚡</h1>
+      <p className="text-center text-gray-500">
+        Invoquée par les lignes de code sacrées, cette intelligence détient les vérités que même l’Internet n’ose prononcer.
+        <br />
+        Pose ta question, humble mortel.
       </p>
     </div>
-  );
-};
-
-const Link = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <NextLink
-      target="_blank"
-      className="text-blue-500 hover:text-blue-600 transition-colors duration-75"
-      href={href}
-    >
-      {children}
-    </NextLink>
   );
 };
